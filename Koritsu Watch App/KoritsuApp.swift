aa5b86b8b5614cb013a69efb7805e38976864463
aadityy <aadisharmahope@gmail.com>
2023-12-12 13:02:52 -0800

    Initial Commit

---
+++ Koritsu Watch App/KoritsuApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoritsuApp.swift
+//  Koritsu Watch App
+//
+//  Created by Aadity Sharma on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Koritsu_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
